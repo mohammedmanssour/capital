df5d4a84064cdea622e235605aa34265d01f3cd5
Mohammed Manssour <manssour.mohammed@gmail.com>
2017-06-19 17:54:11 +0300

    JsonException

---
+++ src/Exceptions/JsonException.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Helilabs\Capital\Exceptions;
+
+Class JsonException extends \Exception
+{
+    /**
+     * Json encodes the message and calls the parent constructor.
+     *
+     * @param null           $message
+     * @param int            $code
+     * @param Exception|null $previous
+     */
+    public function __construct($message = null, $code = 0, Exception $previous = null)
+    {
+        parent::__construct(json_encode($message), $code, $previous);
+    }
+
+    /**
+     * Returns the json decoded message.
+     *
+     * @return mixed
+     */
+    public function getDecodedMessage()
+    {
+        return json_decode($this->getMessage());
+    }
+}
